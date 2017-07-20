6bf2162e4ce12e9a1b37191de5736d5abdec1eb7
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-07-20 10:08:58 -0500

    setting up action creators and reducer

--- src/ducks/user.js
+++ src/ducks/user.js
@@ -1,6 +1,9 @@
 import axios from 'axios'
 const initialState = {
-
+  name: '',
+  user: {},
+  fetching: false,
+  error: null
 }
 
 const ADD_NAME = 'ADD_NAME'
@@ -9,10 +12,23 @@ const GET_DATA_PENDING = 'GET_DATA_PENDING'
 const GET_DATA_FULFILLED = 'GET_DATA_FULFILLED'
 const GET_DATA_REJECTED = 'GET_DATA_REJECTED'
 
+// action creators the user can call
 export function addName(name) {
+  return {
+    type: ADD_NAME,
+    payload: name
+  }
 }
 export function getData() {
 }
 
+//makes the changes based on what the user does
 export default function userReducer(state=initialState, action) {
+  switch (action.type) {
+    case ADD_NAME:
+      let newState = Object.assign({}, state, {name: action.payload})
+      return newState
+    default:
+      return state
+  }
 }
--- src/store.js
+++ src/store.js
@@ -1,4 +1,6 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
-import promiseMiddleWare from 'redux-promise-middleware'
+// import promiseMiddleWare from 'redux-promise-middleware'
 
-
+//import reducer
+import user from './ducks/user'
+export default createStore(user)
